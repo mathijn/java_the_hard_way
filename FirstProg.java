5b8072b25a6e73b3b409d168c155a678edafcd83
Mathijn van Dijk <mathijnvandijk@outlook.com>
2017-03-23 20:16:31 +0100

    test from Cmder

--- FirstProg.java
+++ FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
 	public static void main (String[] args){
 		System.out.println("Iam mathijn");
-		System.out.println("today's date is");
+		System.out.println("today's date is 23 maart");
 	} 
 }
