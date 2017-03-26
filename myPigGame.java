f180493e355c7780e46f008c27565b408202b1a3
Mathijn van Dijk <mathijnvandijk@outlook.com>
2017-03-26 17:52:02 +0200

    myPigGame bijna klaar

--- myPigGame.java
+++ myPigGame.java
@@ -7,48 +7,69 @@ public class myPigGame{
 
 	public static void main(String[] args) {
 		Scanner key = new Scanner(System.in);
-		String fPlayer;
-		System.out.print("C for comp, P for player: ");
-		fPlayer = key.next().toLowerCase();
-		if(fPlayer.equals("c")){
+		boolean stop;
+		System.out.print("Computer is goign first: ");
+		do{
 			myPigGame.computerPlays();
-		}else{
-			System.out.println("Player plays");
-			myPigGame.playerPlays();
-			if(myPigGame.playerPlays()){
-				// myPigGame.computerPlays();
-				System.out.println("COMPUTERRRR");
+			if(counterComp < 30){
+				myPigGame.playerPlays();
+			}else{
+				System.out.println("\n\n\nCOMPUTER WINS!!");
 			}
-		}
+			if(counterPlayer < 30 ){
+				myPigGame.computerPlays();
+			}else{
+				System.out.println("\n\n\n PLAYERRRR WINS!!");
+			}
+		}while(counterPlayer < 30 || counterComp < 30);
 	}
 
-	public static int computerPlays(){
-		System.out.println("Computer plays");
-		counterComp = counterComp++;
-		return counterComp;
-	}
-
-	public static boolean playerPlays(){
-		String again = "y";
+	public static void computerPlays(){
+		int again=0;
 		Scanner key = new Scanner(System.in);
+		System.out.println("Computer plays");
 		do{
 			dice = myPigGame.randomDice();
-			counterPlayer = counterPlayer+=dice;
-			System.out.println("\tYou rolled: "+dice);
-			if(dice == 1){
-				again = "n";
-				counterPlayer = 0;
-				System.out.print("\tOuch! you rolled 1, which means.. ");
-				return true;
+			counterComp = counterComp+=dice;
+			again = again+=dice;
+			System.out.println("\n\tComputer rolled: "+dice);
+			if(dice != 1){
+				System.out.println("\tComputer scored: "+counterComp);
 			}else{
-				System.out.println("\tYou scored: "+counterPlayer);
-				System.out.print("\nDo you want to roll again? (Y/N)");
-				again = key.next().toLowerCase();
-				again = "y";
+				System.out.println("\n\tOuch! computer rolled 1, which means.. ");
+				counterComp = 0;
 			}
-		}while(again.equals("y") && dice != 1);
-		System.out.println("Your score is: "+counterPlayer);
-		return true;
+		}while(again < 20 && dice != 1);
+		System.out.println("\tComputer's total score is: "+counterComp);
+		System.out.println("------------------------------");
+	}
+
+	public static void playerPlays(){
+		String again;
+		Scanner key = new Scanner(System.in);
+		System.out.println("\nPlayer plays");
+		System.out.print("Are you ready? (Y/N): ");
+		String ready = key.next().toLowerCase();
+		if(ready.equals("n")){
+			myPigGame.playerPlays();
+		}else{
+			do{
+				dice = myPigGame.randomDice();
+				counterPlayer = counterPlayer+=dice;
+				System.out.println("\n\tYou rolled: "+dice);
+				if(dice != 1){
+					System.out.println("\tYou scored: "+counterPlayer);
+					System.out.print("\nDo you want to roll again? (Y/N): ");
+					again = key.next().toLowerCase();
+				}else{
+					System.out.println("\tOuch! you rolled 1, which means.. ");
+					again = "n";
+					counterPlayer = 0;
+				}
+			}while(again.equals("y") && dice != 1);
+		}
+		System.out.println("\tYour total score is: "+counterPlayer);
+		System.out.println("------------------------------");
 	}
 
 	public static int randomDice(){
